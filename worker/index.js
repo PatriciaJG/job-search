64845f21e2da459afdd1c3b8eaa80158a7b33f31
Patricia Jolivert <patricia.jolivert@gmail.com>
2020-06-16 19:44:49 +0200

    Page set up + components set up + github API setup

---
+++ worker/index.js
@@ -0,0 +1,8 @@
+//Runs a node process where we are importing the library
+//And then
+var CronJob = require('cron').CronJob
+
+const fetchGithub = require('./tasks/fetch-github.js')
+
+//fetch github jobs every minute
+new CronJob('* * * * * *', fetchGithub, null, true, 'America/Los_Angeles')
